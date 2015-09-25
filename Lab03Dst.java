f85f7cd41d9bb3d881c4474609cbcf32362d6a1b
unknown <4069760@HS222-16.vtsd.com>
2015-09-25 11:56:16 -0400

    Lab03

---
+++ Lab03Dst.java
@@ -0,0 +1,60 @@
+/************************************
+**          Lucas Rotun            **
+** Lab03Dst - This lab demonstrate **
+**     the use of variables and    **
+**   mathematical expressions in   ** 
+**      a practical program.       **
+************************************/
+
+public class Lab03Dst
+{
+	public static void main(String args[])
+	{
+		System.out.println("Lab 03D");
+		System.out.println();
+		System.out.println("----------------------");
+		int num1 = 11;
+		int num2 = 22;
+		int num3 = 33;
+		int num4 = 44;
+		int num5 = 55;
+		System.out.println("1st integer:" + num1);
+		System.out.println("2nd integer:" + num2);
+		System.out.println("3rd integer:" + num3);
+		System.out.println("4th integer:" + num4);
+		System.out.println("5th integer:" + num5);
+		System.out.println("----------------------");
+		System.out.println();
+		int Total = 165;
+		int Average = 33;
+		System.out.println("Integer Total:" + Total);
+		System.out.println("Integer Average:" + Average);
+		System.out.println("----------------------");
+		System.out.println();
+		double num6 = 55.55;
+		double num7 = 66.66;
+		double num8 = 77.77;
+		double num9 = 88.88;
+		double num10 = 99.99;
+		System.out.println("1st real number:" + num6);
+		System.out.println("2nd real number:" + num7);
+		System.out.println("3rd real number:" + num8);
+		System.out.println("4th real number:" + num9);
+		System.out.println("5th real number:" + num10);
+		System.out.println("----------------------");
+		System.out.println();
+		double DTotal = 388.85;
+		double DAverage = 77.77000000000001;
+		System.out.println("Double Total:" + DTotal);
+		System.out.println("Double Average:" + DAverage);
+		System.out.println("----------------------");
+		System.out.println();
+
+
+
+
+
+
+		System.out.println();
+	}
+}
